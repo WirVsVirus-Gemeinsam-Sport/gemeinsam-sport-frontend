476206960ae3fb26e63e0075a53d72aba2c054a7
Erik Schilling <git@ablu.org>
2020-03-22 16:21:14 +0100

    Set default workout

--- lib/gemeinsam_sport_frontend/room.ex
+++ lib/gemeinsam_sport_frontend/room.ex
@@ -42,7 +42,12 @@ defmodule GemeinsamSportFrontend.Room do
     GemeinsamSportFrontend.RoomRegistry.register(id)
     {:ok, %State{
       id: id,
-      workout: []
+      workout: [
+        %{id: 1, type: "Warmup", duration: 5},
+        %{id: 2, type: "Do a plank", duration: 30},
+        %{id: 3, type: "Break", duration: 10},
+        %{id: 4, type: "Push Ups", duration: 30},
+      ]
     }}
   end
 
